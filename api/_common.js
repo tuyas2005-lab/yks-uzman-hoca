a8297bbf2c2939db08af3b1d29c376117459b4d6
tuyas2005-lab <tuyas2005@gmail.com>
2026-08-18 21:16:22 +0300

    Add dedicated fast solve model

--- api/_common.js
+++ api/_common.js
@@ -4,6 +4,7 @@ import OpenAI from "openai";
 
 export const MODEL = process.env.OPENAI_MODEL || "gpt-5.6";
 export const FAST_MODEL = process.env.OPENAI_FAST_MODEL || "gpt-5-mini";
+export const SOLVE_MODEL = process.env.OPENAI_SOLVE_MODEL || "gpt-4.1-mini";
 export const officialDomains = ["tymm.meb.gov.tr","mebi.eba.gov.tr","ogmmateryal.eba.gov.tr","osym.gov.tr"];
 export const counselorDomains = ["orgm.meb.gov.tr","meb.gov.tr","mebi.eba.gov.tr"];
 

c0c48fe28942b161082a60f2222f1e71dc498003
tuyas2005-lab <tuyas2005@gmail.com>
2026-08-18 22:41:04 +0300

    Cache SAY EA field selection layer

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'yks-uzman-hoca-v4.8.12-shell';
+const CACHE = 'yks-uzman-hoca-v4.8.13-shell';
 const SHELL = [
   '/',
   '/index.html',
@@ -16,7 +16,8 @@ const SHELL = [
   '/app-yks-coach.js',
   '/app-yks-coach-fix.js',
   '/app-home-data.js',
-  '/app-home-links.js'
+  '/app-home-links.js',
+  '/app-field-track.js'
 ];
 
 self.addEventListener('install', event => {
